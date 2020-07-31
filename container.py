98197fba2652fb3abc9c93508436134494b75d59
Vandlol <max@henzemail.org>
2020-07-31 15:12:22 +0200

    WIP

--- container.py
+++ container.py
@@ -7,43 +7,85 @@ from pprint import pprint as pp
 
 class Container:
     def __init__(self):
-        pass
+        self.config = misc.read_config()
+        self.numerical_properties = [
+            "containerimx", "containerdimy", "containerdimz", "containeremptyweight"]
+        self.id_properties = ["containerparent",
+                              "containerid", "containerconstraints"]
+        self.string_properties = ["containertype", "containername"]
+        self.year_properties = ["containeryearadded"]
 
     def new(self, container):
         self.container = container
         if not self._verify_container():
             # TODO error handling
             print("there was an error")
-        if not self.container["containerlabel"]:
+        exit()
+        if not self.container["containerid"]:
             # TODO make configurable
-            self.container["containerlabel"] = misc.gen_id()
-            while item_classes.Templates.find_one({"containerlabel": self.container["containerlabel"]}):
-                self.container["containerlabel"] = misc.gen_id()
+            self.container["containerid"] = misc.gen_id(
+                id_length=self.config["id_length"])
+            while item_classes.Templates.find_one({"containerid": self.container["containerid"]}):
+                self.container["containerid"] = misc.gen_id(
+                    id_length=self.config["id_length"])
 
         self.container["containercontent"] = dict()
 
     def _verify_container(self):
-        mandatory_properties = ["containertype", "containerlabel"]
+        mandatory_properties = ["containertype", "containerid"]
         err = 0
         for prop in mandatory_properties:
             if not self.container.get(prop):
                 err += 1
-                print("Property {} missing or None".format(prop))
+                print("Container Property {} missing or None".format(prop))
+
+        for prop, val in self.container.items():
+            if not val:
+                continue
+            print("---")
+            print(prop, val)
+            if prop in self.numerical_properties:
+                print("num")
+                if misc.verify_int_or_float(val):
+                    print("ok")
+                    continue
+                else:
+                    err += 1
+                    print(
+                        "Container Property {} is not an integer or float.".format(prop))
+            if prop in self.string_properties:
+                print("string")
+                if misc.verify_string(val):
+                    print("ok")
+                    continue
+                else:
+                    err += 1
+                    print("Container Property {} is not a string.".format(prop))
+            if prop in self.id_properties:
+                print("id")
+                if misc.verify_id(val, id_length=self.config["id_length"]):
+                    print("ok")
+                    continue
+                else:
+                    err += 1
+                    print("Container Property {} is not a valid ID.".format(prop))
+            if prop in self.year_properties:
+                print("year")
+                if misc.verify_year(val):
+                    print("ok")
+                    continue
+                else:
+                    err += 1
+                    print("Container Property {} is not a Year.".format(prop))
+
         if err != 0:
             return(False)
-        numerical_properties = ["containerimx",
-                                "containerdimy", "containerdimz", "containeremptyweight"]
-
-        id_properties = ["containerparent",
-                         "containerlabel", "containerconstraints"]
-        string_properties = ["containertype", "containername"]
-        year_properties = ["containeryearadded"]
-
-    return(True)
+        return(True)
 
 
 class Template:
     def __init__(self, templateid=None, containertype=None, containerimx=None, containerdimy=None, containerdimz=None, containeremptyweight=None, templatename=None, templatetype="container"):
+        self.config = misc.read_config()
         self.templateid = templateid
         self.containertype = containertype
         self.containerimx = containerimx
@@ -54,9 +96,9 @@ class Template:
         self.templatetype = templatetype
 
     def new(self):
-        self.templateid = misc.gen_id()
+        self.templateid = misc.gen_id(id_length=self.config["id_length"])
         while item_classes.Templates.find_one({"templateid": self.templateid}):
-            self.templateid = misc.gen_id()
+            self.templateid = misc.gen_id(id_length=self.config["id_length"])
 
         self.template = {
             "templatetype": self.templatetype,
@@ -102,19 +144,17 @@ class Template:
 
 
 test = {
-    "containertype": None,
-    "containerimx": None,
-    "containerdimy": None,
-    "containerdimz": None,
-    "containeremptyweight": None,
+    "containertype": "container",
+    "containerimx": 9,
+    "containerdimy": 5,
+    "containerdimz": 3,
+    "containeremptyweight": 15,
     "containerparent": None,
-    "containername": None,
-    "containerlabel": None,
-    "containeryearadded": None,
+    "containername": "con1",
+    "containerid": "QR7G",
+    "containeryearadded": 1995,
     "containerconstraints": None,
     "containercontent": dict()
-
-
 }
 
 Container().new(test)
--- misc.py
+++ misc.py
@@ -1,6 +1,8 @@
 import json
 import random
 import string
+import re
+from pprint import pprint as pp
 
 
 def read_config(file="config.json"):
@@ -9,23 +11,33 @@ def read_config(file="config.json"):
     return(config)
 
 
-def gen_id():
+def gen_id(id_length=4):
     id = ''.join(
-        [random.choice(string.ascii_letters.upper() + string.digits) for n in range(4)])
+        [random.choice(string.ascii_letters.upper() + string.digits) for n in range(id_length)])
     return(id)
 
 
-def verify_id():
-    pass
+def verify_id(val, id_length=4):
+    if re.match(r"^[A-Z0-9]{{{}}}$".format(id_length), val):
+        return(True)
+    return(False)
 
 
-def verify_year():
-    pass
+def verify_year(val):
+    if re.match(r'^\d{4}$', str(val)):
+        return(True)
+    return(False)
 
 
-def verify_string():
-    pass
+def verify_string(val):
+    if isinstance(val, str):
+        return(True)
+    return(False)
 
 
-def verify_int_or_float():
-    pass
+def verify_int_or_float(val):
+    if isinstance(val, int):
+        return(True)
+    elif isinstance(val, float):
+        return(True)
+    return(False)
