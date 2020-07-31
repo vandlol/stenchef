5f53e6313a64960b1c9b87cee206c4d2a7fbcc0f
Vandlol <max@henzemail.org>
2020-07-31 23:28:40 +0200

    WIP

--- container.py
+++ container.py
@@ -1,8 +1,9 @@
 from log import logger as l
 import misc
 import item_classes
-import sys
+import template as t
 
+import sys
 from pprint import pprint as pp
 
 
@@ -31,11 +32,13 @@ class Container:
                     id_length=self.config["id_length"])
 
         self.container["containercontent"] = dict()
-        # TODO saving
+        item_classes.Containers(self.container).save()
+        l.info("Created Container with containerid: {}".format(
+            self.container["containerid"]))
         return(self.container["containerid"])
 
     def from_template(self, templateid, container):
-        self.container = Template().load(templateid=templateid)
+        self.container = t.Template().load(templateid=templateid)
         if not self.container:
             l.error("Could not load Container Template: {}.".format(templateid))
             sys.exit("32")
@@ -102,109 +105,28 @@ class Container:
             return(False)
         return(True)
 
-
-class Template:
-    def __init__(self):
-        self.config = misc.read_config()
-        self.mandatory_properties = ["containertype"]
-        self.numerical_properties = [
-            "containerimx", "containerdimy", "containerdimz", "containeremptyweight"]
-        self.string_properties = ["containertype",
-                                  "containername", "templatename"]
-
-    def new(self, template):
-        self.template = template.copy()
-        if not self._verify_template():
-            l.error("There was an error within the Container Template Properties.")
-            sys.exit("41")
-        if not self.template.get("templateid"):
-            self.template["templateid"] = misc.gen_id(
-                id_length=self.config["id_length"])
-            while item_classes.Templates.find_one({"templateid": self.template["templateid"]}):
-                self.template["templateid"] = misc.gen_id(
-                    id_length=self.config["id_length"])
-        check_template = item_classes.Templates.find_one(template)
-        if check_template:
-            l.info("A template containing the given Properties already exists with templateid: {}".format(
-                check_template.__dict__['templateid']))
-            return(check_template.__dict__['templateid'])
-        if not self.template["templatename"]:
-            self.template["templatename"] = self.template["templateid"]
-        item_classes.Templates(self.template).save()
-        return(self.template["templateid"])
-
-    def load(self, templateid=None):
-        if templateid:
-            self.templateid = templateid
-        if not self.templateid:
-            l.error("No templateid given.")
-            return(None)
-        ret_template = item_classes.Templates.find_one(
-            {"templateid": self.templateid})
-        if not ret_template:
-            return(dict())
-        return(ret_template.__dict__)
-
-    def list_type(self, templatetype=None):
-        if templatetype:
-            self.templatetype = templatetype
-        if not self.templatetype:
-            # TODO error handling
-            print("no templatetype given")
-            return(None)
+    def list_items(self, search=None):
         ret_list = list()
-        for item in list(item_classes.Templates.find({"templatetype": self.templatetype})):
-            ret_list.append(item.__dict__)
+        if not search:
+            for item in list(item_classes.Containers.find()):
+                ret_list.append(item.__dict__)
+        else:
+            for item in list(item_classes.Containers.find(search)):
+                ret_list.append(item.__dict__)
         return(ret_list)
 
-    def list_all(self):
-        ret_list = list()
-        for item in list(item_classes.Templates.find()):
-            ret_list.append(item.__dict__)
-        return(ret_list)
-
-    def _verify_template(self):
-        def _log_ok(prop, val):
-            l.debug(
-                "Verification Container Template Property {}, Value {}: OK.".format(prop, val))
-            return
-
-        err = 0
-        for prop in self.mandatory_properties:
-            if not self.template.get(prop):
-                err += 1
-                l.error(
-                    "Container Template Property {} missing or None".format(prop))
-                continue
-            l.debug(
-                "Mandatory Container Template Property {} has been defined".format(prop))
-        if err != 0:
-            l.error(
-                "{} Mandatory Container Template Properties are missing.".format(err))
-            return(False)
-        for prop, val in self.template.items():
-            if not val:
-                continue
-            if prop in self.numerical_properties:
-                if misc.verify_int_or_float(val):
-                    _log_ok(prop, val)
-                    continue
-                else:
-                    err += 1
-                    l.error(
-                        "Container Template Property {} is not an integer or float.".format(prop))
-            if prop in self.string_properties:
-                if misc.verify_string(val):
-                    _log_ok(prop, val)
-                    continue
-                else:
-                    err += 1
-                    l.error(
-                        "Container Template Property {} is not a string.".format(prop))
-        if err != 0:
-            l.debug("Container Template Property Error Count is {}.".format(err))
-            return(False)
-        return(True)
+    def delete(self, containerid):
+        if not containerid:
+            l.error("No containerid given.")
+            sys.exit("36")
+        containers = self.list_items(search={"containerid": containerid})
+        if containers:
+            for container in containers:
+                item_classes.Containers(container).delete()
+                l.info("Deleted Container with containerid: {}".format(containerid))
+            return(True)
+        l.error("Could not find Container with containerid: {}".format(containerid))
+        return(False)
 
 
 test_container = {
@@ -215,7 +137,6 @@ test_container = {
     "containeremptyweight": 15,
     "containerparent": None,
     "containername": "con1",
-    "containerid": "QR7G",
     "containeryearadded": 1995,
     "containerconstraints": None,
 }
@@ -224,7 +145,7 @@ test_container2 = {
     "containerparent": None,
     "containername": "con1",
     "containerid": "QR7G",
-    "containeryearadded": 1995,
+    "containeryearadded": 2020,
     "containerconstraints": None,
 }
 
@@ -237,9 +158,11 @@ test_template = {
     'templatename': "box1"
 }
 
-# pp(Template().list_all())
+# pp(t.Template().list_items())
 # pp(Container().from_template("LIGA", test_container))
-pp(Container().from_template("LIHA", test_container2))
-# pp(Template().new(test_template))
+# pp(Container().from_template("LIHA", test_container2))
+# pp(Container().list_items())
+# pp(Container().delete('QR7G'))
+# pp(t.Template().new(test_template))
 
 # Container().new(test)
