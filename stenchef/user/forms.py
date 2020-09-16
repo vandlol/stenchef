5f0d1c97c61e669f4fe1afc36a5641ff5c34a776
Vandlol <max@henzemail.org>
2020-09-16 19:00:51 +0200

    current WIP

--- stenchef/user/forms.py
+++ stenchef/user/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+from django.utils.translation import ugettext_lazy as _
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(
@@ -13,14 +15,36 @@ class UserRegisterForm(UserCreationForm):
             }
         )
     )
-    username = forms.CharField(
+    # username = forms.CharField(
+    #    widget=forms.TextInput(
+    #        attrs={
+    #            "class": "bg-white rounded border border-gray-400 focus:outline-none #focus:border-teal-500 text-base px-4 py-2 mb-4",
+    #            "placeholder": "Login",
+    #            "id": "username",
+    #            "label": "penis",
+    #        },
+    #    )
+    # )
+    username = forms.RegexField(
+        label=_("Login"),
+        max_length=30,
+        regex=r"^[\w.@+-]+$",
+        help_text=_(
+            "Required. 30 characters or fewer. Letters, digits and " "@/./+/-/_ only."
+        ),
+        error_messages={
+            "invalid": _(
+                "This value may contain only letters, numbers and "
+                "@/./+/-/_ characters."
+            )
+        },
         widget=forms.TextInput(
             attrs={
-                "class": "bg-white rounded border border-gray-400 focus:outline-none focus:border-teal-500 text-base px-4 py-2 mb-4",
+                "class": "bg-white rounded border border-gray-400 focus:outline-none #focus:border-teal-500 text-base px-4 py-2 mb-4",
+                "required": "true",
                 "placeholder": "Login",
-                "id": "username",
             }
-        )
+        ),
     )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
